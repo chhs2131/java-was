01bdb81e967fa619889acf6d869d61c55b6d6bf2
HyeonSik Choi <chgy2131@naver.com>
2024-07-04 12:58:41 +0900

    feat: 회원가입시 이메일을 입력받는다 - HTML 회원가입 Form에 이메일 필드를 추가 - 저장을 위한 User 클래스에 이메일 필드 추가

--- src/main/java/codesquad/model/User.java
+++ src/main/java/codesquad/model/User.java
@@ -2,21 +2,24 @@ package codesquad.model;
 
 public class User {
     private final String name;
+    private final String email;
     private final String password;
     private final String nickname;
 
-    public User(final String name, final String password, final String nickname) {
+    public User(final String name, final String password, final String nickname, final String email) {
         this.name = name;
         this.password = password;
         this.nickname = nickname;
+        this.email = email;
     }
 
     @Override
     public String toString() {
         return "User{" +
-            "name='" + name + '\'' +
-            ", password='" + password + '\'' +
-            ", nickname='" + nickname + '\'' +
-            '}';
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
     }
 }
