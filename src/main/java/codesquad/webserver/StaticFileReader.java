faf5d02c3af6987311658143b25ba6f7cd14d25b
HyeonSik Choi <chgy2131@naver.com>
2024-07-05 09:40:33 +0900

    fix: 배포 환경에서 정적 파일을 불러오지 못하던 문제 수정 - 파일을 고정된 상대경로 기반이 아닌, ClassLoader를 통해 불러온다. - getResourceAsStream을 활용하여 파일을 불러온다.

--- src/main/java/codesquad/webserver/StaticFileReader.java
+++ src/main/java/codesquad/webserver/StaticFileReader.java
@@ -3,30 +3,17 @@ package codesquad.webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class StaticFileReader {
     private static final Logger logger = LoggerFactory.getLogger(StaticFileReader.class);
-    private static final String STATIC_FILE_PATH = "src/main/resources/static";
+    private static final String STATIC_FILE_PATH = "static";
 
     public String read(String path) {
-        String filePath = STATIC_FILE_PATH + path;
-
-        File file = new File(filePath);
-        if (!file.exists()) {
-            throw new IllegalArgumentException("파일이 존재하지 않습니다. path: " + filePath);
-        }
-
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            byte[] fileBytes = fileInputStream.readAllBytes();
-            return new String(fileBytes, "UTF-8");
-        } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("파일을 찾을 수 없습니다. path: " + filePath, e);
+        try (InputStream resource = getClass().getClassLoader().getResourceAsStream(STATIC_FILE_PATH + path)) {
+            return new String(resource.readAllBytes());
         } catch (IOException e) {
-            throw new IllegalArgumentException("파일을 읽는 중 오류가 발생했습니다. path: " + filePath, e);
+            throw new IllegalArgumentException("파일을 읽는 중 오류가 발생했습니다. path: " + path, e);
         }
     }
 }
