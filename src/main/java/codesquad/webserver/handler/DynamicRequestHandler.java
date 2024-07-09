e0b52fabc928fcbec6df03be3ca3edc3e39ac8d2
HyeonSik Choi <chgy2131@naver.com>
2024-07-09 13:13:52 +0900

    refactor: 요청에 따른 핸들러를 DynamicRequestHandler 외부에 정의 - User 회원가입 핸들러를 UserHandler로 분리하고 이를 활용하도록 수정한다.

--- src/main/java/codesquad/webserver/handler/DynamicRequestHandler.java
+++ src/main/java/codesquad/webserver/handler/DynamicRequestHandler.java
@@ -1,11 +1,11 @@
 package codesquad.webserver.handler;
 
+import codesquad.application.UserHandler;
 import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
 import codesquad.http.type.HttpMethod;
 import codesquad.http.type.HttpProtocol;
 import codesquad.http.type.HttpStatus;
-import codesquad.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +18,7 @@ public class DynamicRequestHandler implements RouterHandler {
     private static final Map<HandlerPath, Function<HttpRequest, HttpResponse>> mapping = new HashMap<>();
     static {
         // 외부에서 전달받도록 수정 필요
-        mapping.put(new HandlerPath(HttpMethod.POST, "/create"), DynamicRequestHandler::getCreateUser);
+        mapping.put(new HandlerPath(HttpMethod.POST, "/user/create"), UserHandler::getCreateUser);
     }
 
     @Override
@@ -37,21 +37,5 @@ public class DynamicRequestHandler implements RouterHandler {
         }
 
         return new HttpResponse(HttpProtocol.HTTP_1_1, HttpStatus.INTERNAL_SERVER_ERROR, null, "서버에서 에러가 발생했습니다.");
-    }
-
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    private static HttpResponse getCreateUser(HttpRequest httpRequest) {
-        String name = httpRequest.body().get("name");
-        String password = httpRequest.body().get("password");
-        String nickname = httpRequest.body().get("nickname");
-        String email = httpRequest.body().get("email");
-
-        final User user = new User(name, password, nickname, email);
-        logger.debug("회원가입을 완료했습니다. {}", user);
-
-        return new HttpResponse(HttpProtocol.HTTP_1_1, HttpStatus.FOUND, Map.of("Location", "/index.html"), "유저가 생성되었습니다.");
     }
 }
