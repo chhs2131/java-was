2fad49de8d18c2830d65f12fd5cb5d59b0aad7bf
Basak <chgy2131@naver.com>
2024-07-13 17:25:50 +0900

    feat: Filter 인터페이스 추가 - Filter 기능을 해당 인터페이스를 implements하여 구현한다.

--- src/main/java/codesquad/webserver/HttpTask.java
+++ src/main/java/codesquad/webserver/HttpTask.java
@@ -1,14 +1,10 @@
 package codesquad.webserver;
 
-import codesquad.application.User;
-import codesquad.webserver.authentication.AuthenticationHolder;
+import codesquad.webserver.filter.SessionFilter;
 import codesquad.webserver.http.HttpRequest;
 import codesquad.webserver.http.parser.HttpRequestParser;
 import codesquad.webserver.http.HttpResponse;
 import codesquad.webserver.http.ResponseConverter;
-import codesquad.webserver.http.type.Cookie;
-import codesquad.webserver.session.Session;
-import codesquad.webserver.session.SessionManager;
 import codesquad.webserver.socket.SocketReader;
 import codesquad.webserver.socket.SocketWriter;
 import org.slf4j.Logger;
@@ -16,7 +12,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Optional;
 
 /**
  * 전달 받은 Socket을 기반으로 HTTP 요청 해석 및 적절한 응답을 진행합니다.
@@ -38,12 +33,9 @@ public record HttpTask(Socket clientSocket, RequestHandler requestHandler) imple
             logger.debug("HTTP Headers: size: {}", request.headers().size());
             logger.debug("HTTP Body: {}", request.body());
 
-
-            SessionFilter sessionFilter = new SessionFilter();
-            if (sessionFilter.support()) {
-                sessionFilter.doFilter(request);
-            }
-
+            // TODO Filter 묶음을 만들고 그곳에 사용할 필터들을 등록한뒤 처리할 것
+            final SessionFilter sessionFilter = new SessionFilter();
+            request = sessionFilter.preFilter(request);
 
             HttpResponse response = requestHandler.handle(request);
             byte[] socketBytes = ResponseConverter.toSocketBytes(response);
@@ -53,36 +45,6 @@ public record HttpTask(Socket clientSocket, RequestHandler requestHandler) imple
         } catch (IOException e) {
             logger.error("요청 처리를 실패했습니다. {}", e.getMessage());
             e.printStackTrace();
-        }
-    }
-
-    ////////////////// TODO Filter 흉내내기!
-    private static class SessionFilter {
-        private static final SessionManager sessionManager = new SessionManager();
-
-        public boolean support() {
-            return true;
-        }
-
-        public void doFilter(HttpRequest httpRequest) {
-            // ThreadLocal 초기화
-            AuthenticationHolder.clear();
-
-            // 세션 정보 가져오기
-            Cookie cookies = httpRequest.headers().getCookies();
-            String sessionId = cookies.get("SID");
-
-            // 세션이 정상인지 확인
-            if (sessionId == null || !sessionManager.validSession(sessionId)) {
-                return;
-            }
-
-            // 정상인 경우 ThreadLocal에 이를 저장
-            Optional<Session> session = sessionManager.getSession(sessionId);
-            User user = (User) session.get().attributes().get("user");
-            AuthenticationHolder.setContext(user);
-
-            // TODO 끝나고 ThreadLocal 비우기
         }
     }
 }
