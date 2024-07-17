dad45c4fd588c74fcdc55d5f3819dd1ecc39d3cd
Basak <chgy2131@naver.com>
2024-07-17 23:25:19 +0900

    feat: MultiPartParser 추가 - 전달된 Bytes에서 MultiPart Body만 추출해서 Map<String,String> 형태로 반환한다. - Body에 전달된 값이 File(이미지 등) 형태인 경우 파일을 저장하고 Path 값을 반환한다.

--- src/main/java/codesquad/webserver/http/parser/BodyParser.java
+++ src/main/java/codesquad/webserver/http/parser/BodyParser.java
@@ -1,49 +1,16 @@
 package codesquad.webserver.http.parser;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BodyParser {
+    private static final Logger logger = LoggerFactory.getLogger(BodyParser.class);
     private BodyParser() {}
 
     public static String parse(String[] lines, int contentLength) {
         return extractBody(lines, contentLength);
     }
-
-    public static Map<String, String> parseFormMultiPart(String[] lines, int contentLength, String boundary) {
-        String body = extractBodyOnlyLength(lines, contentLength);
-        String[] multiPart = body.split(boundary);
-
-        for (String s : multiPart) {
-            System.out.println("Target: " + s);
-            parseMultipartData(s);
-        }
-
-        throw new UnsupportedOperationException();
-    }
-
-    private static void parseMultipartData(String line) {
-        Map<String, String> multiPart = KeyValueParser.parseMultiPart(line);
-        System.out.println("헤더반환: " + multiPart);
-
-        // Header
-
-        // Body
-
-    }
-
-    private static void saveFile(byte[] content, String fileName) {
-        try (FileOutputStream fos = new FileOutputStream(new File(fileName))) {
-            fos.write(content);
-            System.out.println("File saved: " + fileName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
 
     public static Map<String, String> pasreFormXwww(String[] lines, int contentLength) {
         String body = extractBody(lines, contentLength);
@@ -64,36 +31,6 @@ public class BodyParser {
 
         if (body.length() > contentLength) {
             body.setLength(contentLength);
-        }
-
-        return body.toString();
-    }
-
-    private static String extractBodyOnlyLength(String[] lines, int contentLength) {
-        StringBuilder body = new StringBuilder();
-
-        int bodyStartIndex = getBodyStartIndex(lines);
-        int currentLength = 0;
-
-        for (int i = bodyStartIndex; i < lines.length; i++) {
-            String line = lines[i];
-            int lineLength = line.length() + 1; // '\n' 문자를 고려하여 1을 더함
-
-            if (currentLength + lineLength > contentLength) {
-                int remainingLength = contentLength - currentLength;
-                body.append(line, 0, Math.min(remainingLength, line.length()));
-                if (remainingLength > line.length()) {
-                    body.append('\n');
-                }
-                break;
-            } else {
-                body.append(line).append('\n');
-                currentLength += lineLength;
-            }
-        }
-
-        if (body.length() > 0 && body.charAt(body.length() - 1) == '\n') {
-            body.deleteCharAt(body.length() - 1);
         }
 
         return body.toString();
